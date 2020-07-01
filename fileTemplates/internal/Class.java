83657a23125b86935054cf554edf1c3118c9cc6c
zhouxiangfu <zxf@is-keen.com>
2020-07-01 12:39:36 +0800

    IU-2018.3.5 <zhouxiangfu@zhouxiagfudembp Create baseRefactoring.xml, markdown.xml, alibaba-toolkit-tips.xml, ide.general.xml, CodeGlance.xml, project.default.xml, rainbow_brackets.xml, diff.xml, Enum.java, lombok-plugin.xml, Default.xml, other.xml, yiiguxing.translation.xml, Default.xml, generic-key-value-store.xml, ignore.xml, find.xml, ui.lnf.xml, filetypes.xml, projectView.xml, qaPlugPluginUpdateNotifier.xml, remote-servers.xml, colors.scheme.xml, path.macros.xml, databaseDrivers.xml, templates.xml, GrepConsole.xml, Default.xml, OGNL _Struts 2_.xml, Class.java, jdk.table.xml, laf.xml, MyBatisCodeHelper.xml, vcs.xml, editor.codeinsight.xml, alibaba-toolkit-account.xml, Mybatis_SQL.xml, OGNL.xml, editor.xml, custom.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,15 @@
+#parse("File Header.java")
+
+#if (${PACKAGE_NAME} &&${PACKAGE_NAME} !="")package ${PACKAGE_NAME};#end
+
+/**
+*@ClassName ${NAME}
+*@Description: TODO
+*@Author ${USER}
+*@Date ${DATE} ${TIME}
+*@Version V1.0
+**/
+
+public class ${NAME} {
+
+}
